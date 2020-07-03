08da35be51565d4885e68f2378ee35ad97b69a09
Gianluca <gianlucazuccarelli@gmail.com>
2020-07-03 20:53:43 +0100

    use traverse js for cache updates

--- src/utils/mutationOptions.js
+++ src/utils/mutationOptions.js
@@ -1,3 +1,5 @@
+import traverse from 'traverse';
+
 import { FIND_TODOS } from "../gql/queries";
 
 export const createTodoUpdateQuery = (proxy, { data: { createTodo }}) => {
@@ -17,32 +19,28 @@ export const createTodoUpdateQuery = (proxy, { data: { createTodo }}) => {
 };
 
 export const updateTodoUpdateQuery = (proxy, { data: { updateTodo }}) => {
-  const data = proxy.readQuery({ query: FIND_TODOS });
-  const items = data.findTodos.items.map(item => {
-    if (item.id === updateTodo.id) return updateTodo;
-    return item;
+  const cached = proxy.readQuery({ query: FIND_TODOS });
+  // eslint-disable-next-line
+  const data = traverse(cached).map(function(item) {
+    // function required to preserve `this` keyword
+    if (item.id === updateTodo.id) this.update(updateTodo);
   });
+
   proxy.writeQuery({ 
     query: FIND_TODOS,
-    data: {
-      findTodos: {
-        ...data.findTodos,
-        items,
-      },
-    }
+    data
   })
 };
 
 export const deleteTodoUpdateQuery = (proxy, { data: { deleteTodo }}) => {
-  const data = proxy.readQuery({ query: FIND_TODOS });
-  const items = data.findTodos.items.filter(item => item.id !== deleteTodo.id);
+  const cached = proxy.readQuery({ query: FIND_TODOS });
+  // eslint-disable-next-line
+  const data = traverse(cached).map(function(item) {
+    // function required to preserve `this` keyword
+    if (item.id === deleteTodo.id) this.remove();
+  });
   proxy.writeQuery({ 
     query: FIND_TODOS,
-    data: {
-      findTodos: {
-        ...data.findTodos,
-        items,
-      },
-    }
+    data
   })
 };
