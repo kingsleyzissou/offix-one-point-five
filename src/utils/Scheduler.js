0081e0c0080685791023898a113197e8ba819091
Gianluca <gianlucazuccarelli@gmail.com>
2020-07-03 17:08:50 +0100

    apply optimistic responses to offline data

--- src/utils/Scheduler.js
+++ src/utils/Scheduler.js
@@ -1,4 +1,4 @@
-import { from } from 'rxjs';
+import { from, throwError } from 'rxjs';
 
 import { Queue } from './Queue';
 import { NetworkStatus } from './NetworkStatus';
@@ -10,10 +10,16 @@ export class Scheduler {
 
   constructor(client) {
     this.client = client;
+    this.cache = client.cache;
+    this.queryManager = client.queryManager;
+    this.queries = client.queryManager.queries;
+    this.mutationStore = client.queryManager.mutationStore;
     this.offlineQueue = new Queue();
     this.networkStatus = new NetworkStatus();
     this.networkStatus.subscribe(this.initOnlineState.bind(this));
     this.offlineQueue.onRequeue(this.reexecute.bind(this));
+    this.offlineQueue.queued(this.addOR.bind(this));
+    this.offlineQueue.onDequeued(this.dequeueObserver.bind(this));
   }
 
   /**
@@ -36,7 +42,9 @@ export class Scheduler {
    * @param options 
    */
   execute(options) {
-    this.networkGate(options);
+    if (!this.networkStatus.isOnline) {
+      return this.networkGate(options);
+    }
     const { query, variables } = options;
     return from(this.client.mutate({
       mutation: query,
@@ -58,9 +66,18 @@ export class Scheduler {
    */
   reexecute({ payload }) {
     const { entry, key } = payload;
-    const options = { ...entry, retry: entry.retry + 1 }
+    // remove optimistic response before
+    // reexecute since optimistic response
+    // has already been applied
+    const { optimisticResponse, ...opts } = entry;
+    const options = { ...opts, retry: entry.retry + 1 };
     this.execute(options).subscribe(
-      () => this.offlineQueue.dequeue(key), 
+      (result) => {
+        // console.log(this.cache);
+        this.offlineQueue.dequeue(key, result);
+        // const data = this.cache.readQuery({ query: FIND_TODOS });
+        // console.log(data);
+      }, 
       this.offlineQueue.failure
     );
   }
@@ -71,24 +88,79 @@ export class Scheduler {
    * to the network
    * 
    */
-  networkGate({ query, variables, retry = 0 }) {
-    if (!this.networkStatus.isOnline) {
-      console.log('some stuff here');
-      this.offlineStore.add('id' + Math.random(), {
-        query, variables, retry
-      });
-      throw new Error('Added to offline queue');
-    }
+  networkGate(options) {
+    const { query, variables, retry = 0 } = options;
+    this.offlineQueue.enqueue({
+      query, variables, retry, ...options
+    });
+    return throwError('Added to offline queue');
   }
 
-  
+  addOR({ payload }) {
+    console.log(payload);
+    const { query, variables } = payload.value;
+    this.mutationStore.initMutation(
+      payload.key,
+      query,
+      variables
+    );
 
-  
+    if (payload.value.optimisticResponse) {
+      const { optimisticResponse } = payload.value;
+      const optimistic = typeof optimisticResponse === 'function'
+        ? optimisticResponse(variables)
+        : optimisticResponse;
+      
+      this.cache.recordOptimisticTransaction(cache => {
+        markMutationResult({
+          mutationId: payload.key,
+          result: { data: optimistic },
+          document: query,
+          variables: variables,
+          update: payload.value.update,
+        }, cache);
+      }, payload.key);
+    }
+    this.queryManager.broadcastQueries();
+  }
+
+  dequeueObserver({ payload }) {
+    console.log(payload);
+    const mut = this.mutationStore.get(payload.key);
+    console.log(mut);
+  }
 
   retryGate(count) {
+    // TODO limit the number of replication
+    // retries
     if (count > this.retries) {
       throw new Error('max retries attempted');
     }
   }
 
+}
+
+
+function markMutationResult(
+  mutation,
+  cache,
+) {
+  const cacheWrites = [{
+    result: mutation.result.data,
+    dataId: 'ROOT_MUTATION',
+    query: mutation.document,
+    variables: mutation.variables,
+  }];
+
+  cache.performTransaction(c => {
+    cacheWrites.forEach(write => c.write(write));
+
+    // If the mutation has some writes associated with it then we need to
+    // apply those writes to the store by running this reducer again with a
+    // write action.
+    const { update } = mutation;
+    if (update) {
+      update(c, mutation.result);
+    }
+  });
 }
